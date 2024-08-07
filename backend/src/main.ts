2b5cfba8b978d12dafde592a28fd78d651303ea6
madao <gn00678465@gmail.com>
2024-08-07 13:43:17 +0800

    build: add Dockerfile and compose.yaml

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -20,6 +20,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(13000);
+  await app.listen(13000, '0.0.0.0');
 }
 bootstrap();
